66e2d6d85953117cc6f346d20b5b1398856820d9
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-07-30 17:01:08 +0200

    Change styles.css to global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Titlebar from "./layout/Titlebar.jsx";
 import Board from "./layout/Board.jsx";
 import KanbanProvider from "./context/KanbanProvider.jsx";
-import "./styles/styles.css";
+import "./styles/global.css";
 
 export default function App() {
     return (
         <KanbanProvider>
             <Titlebar />
-            <div className="container">
-                <Board />
-            </div>
+            <Board />
         </KanbanProvider>
     );
 }
