ebf47a1b2ef03a93ef279a858dafac0b3a0c822f
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-07-30 17:02:44 +0200

    Add a new className

--- src/layout/Titlebar.jsx
+++ src/layout/Titlebar.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/Titlebar.module.css";
 
 export default function Titlebar() {
     return (
-        <header>
+        <header class="header">
             <h1>Kanban</h1>
         </header>
     );
