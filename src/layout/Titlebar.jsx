0038b9770fa744959961f98fbdb083c3fb68db22
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-07-31 18:09:45 +0200

    Add titlebar

--- src/layout/Titlebar.jsx
+++ src/layout/Titlebar.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { KanbanContext } from "../context/KanbanContext.js";
 import styles from "../styles/Titlebar.module.css";
 
 export default function Titlebar() {
+    const { boardName, setBoardName } = useContext(KanbanContext);
+
     return (
         <header className={styles.header}>
-            <h1>Kanban</h1>
+            <div className={styles.container}>
+                <h1>Kanban</h1>
+                <div>{boardName}</div>
+            </div>
         </header>
     );
 }
